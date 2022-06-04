eb61491b2cff5ce7e0b33edc3d0c7b1b6e997798
Yaro <yshmarov@gmail.com>
2022-06-04 14:08:23 +0200

    Update wicked_pdf.rb

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -10,7 +10,12 @@
 
 WickedPdf.config ||= {}
 WickedPdf.config.merge!({
-                          layout: 'pdf.html.erb'
+                          layout: 'pdf.html.erb',
+                          orientation: 'Landscape', # Portrait
+                          page_size: 'A4',
+                          lowquality: true,
+                          zoom: 1,
+                          dpi: 75
                         })
 
 # WickedPdf.config = {
