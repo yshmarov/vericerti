d6219a476ad7d79534dd0235ecce253e28a7c5c7
Yaro <yshmarov@gmail.com>
2022-06-04 11:09:58 +0200

    install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/landing_page'
-  get 'static_pages/pricing'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  root 'static_pages#landing_page'
+  get 'pricing', to: 'static_pages#pricing'
 end
