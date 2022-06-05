4da6358cde7f7f6ded02dc9c38a6167e66792fea
Yaro <yshmarov@gmail.com>
2022-06-05 02:24:05 +0200

    style certificate email

--- app/mailers/certificate_mailer.rb
+++ app/mailers/certificate_mailer.rb
@@ -1,14 +1,9 @@
 class CertificateMailer < ApplicationMailer
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.certificate_mailer.certificate.subject
-  #
   def certificate
     @certificate = params[:certificate]
-    # @greeting = 'Hi'
 
     mail to: 'to@example.org',
-         subject: 'a new certificate'
+         subject: "#{@certificate.event.name} certificate",
+         cc: @certificate.event.user.email
   end
 end
