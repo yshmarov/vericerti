8c905c0a31eb431cb4615b44fb928b93e64a984d
Yaro <yshmarov@gmail.com>
2022-06-05 02:35:20 +0200

    add more validation fields to events(templates)

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,15 @@
 class Event < ApplicationRecord
+  # Ideally: a user_template that also belongs_to template
   belongs_to :user
   has_many :certificates, dependent: :destroy
 
+  # move to templates model or controller
   CERTIFICATE_TEMPLATES = %i[classic modern language_certificate]
 
   validates :name, presence: true
   validates :description, presence: true
   validates :certificate_template, presence: true
+  validates :organization_name, presence: true
+  validates :website_url, presence: true
+  validates :logo_url, presence: true
 end
